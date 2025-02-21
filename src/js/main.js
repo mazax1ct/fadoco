7ad481ac3c7a934166d90440ef75a4d3c608d0d6
mazax1ct <vova@giveandget.ru>
2025-02-21 19:15:32 +0300

    news

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,62 @@
-$(document).ready(function() {
+//отслеживание скролла для шапки
+var header = $('.header'),
+    scrollPrev = 0;
 
+var resize_scroll = function(e) {
+  var scrolled = $(window).scrollTop();
+
+  if (scrolled > $('.header').height() * 2) {
+		header.addClass('is-scrolled');
+	}
+
+  if (scrolled == 0) {
+		header.removeClass('is-scrolled');
+	}
+
+  if ( scrolled > $('.header').height() && scrolled > scrollPrev ) {
+		header.addClass('is-out');
+	} else {
+		header.removeClass('is-out');
+	}
+
+	scrollPrev = scrolled;
+};
+
+$(document).ready(function() {
+  //запуск функции навешивания класса на шапку
+  resize_scroll();
+
+  $('.js-news-slider').each(function(index, el) {
+    var slider = el.children[1];
+
+    new Swiper(slider, {
+      loop: true,
+      spaceBetween: 24,
+      breakpoints: {
+        768: {
+          slidesPerView: 2
+        },
+        1024: {
+          slidesPerView: 3
+        }
+      },
+
+      navigation: {
+        nextEl: '.js-cards-slider-next[data-slider="'+el.dataset.slider+'"]',
+        prevEl: '.js-cards-slider-prev[data-slider="'+el.dataset.slider+'"]',
+      },
+
+      pagination: {
+        el: '.js-cards-slider-pagination[data-slider="'+el.dataset.slider+'"]',
+        type: 'bullets',
+        clickable: true
+      },
+    });
+  });
 });
+
+//перезапуск функции навешивания класса на шапку при скролле и ресайзе
+$(window).on("scroll", resize_scroll).on("resize", resize_scroll);
 
 $(document).on('click', '.js-menu-toggler', function () {
   var _this = $(this);
